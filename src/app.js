962e9a3f4da03f3a2a07204fdc5f9eba97a99c5d
MilanPeter <milanpeter01@gmail.com>
2024-08-05 10:49:16 +0000

    last done

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const path = require('path');
+const mustacheExpress = require('mustache-express');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages/index.html'));
